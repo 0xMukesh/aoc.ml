fe654880c69a96d38b2e1df5282b99b74a6be11c
Mukesh <mukesh@0xc84.fyi>
2026-09-13 20:15:41 +0530

    feat: taking input from stdin

--- main.ml
+++ main.ml
@@ -1,6 +1,6 @@
 open Stdio
 
-let () =
+let demo_01 () =
   let p1 = Point2d.create (0., 0.) in
   let p2 = Point2d.create (3., 4.) in
   let circle = Geometry.{ center = p1; radius = 3.0 } in
@@ -11,4 +11,23 @@ let () =
   printf
     "is within circle: %b\n"
     (Geometry.is_inside_scene_element p2 (Geometry.Circle circle))
+;;
+
+let rec demo_02 accum =
+  match In_channel.input_line In_channel.stdin with
+  | None -> accum
+  | Some l ->
+    (match Float.of_string_opt l with
+     | None -> accum
+     | Some n -> demo_02 (accum +. n))
+;;
+
+let () =
+  (* %! is a flush directive for ocaml's printf *)
+  printf "choose either 1 or 2\n%!";
+  match In_channel.input_line In_channel.stdin with
+  | None -> ()
+  | Some "1" -> demo_01 ()
+  | Some "2" -> printf "%f\n" (demo_02 0.)
+  | Some c -> printf "unknown option: %s\n" c
 ;;
